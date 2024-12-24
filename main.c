a00bc069fac3c19e44f20fa62b6e0b8598b033ab
Rfellipe <felliperibeiro1310@gmail.com>
2024-12-24 11:32:38 -0300

    added a help message

--- main.c
+++ main.c
@@ -8,9 +8,21 @@
 static int verbose_flag;
 static int post_flag;
 
+void usage(void) {
+  printf("petito [options] <url>\n"
+      "-h, --help       Print this message\n"
+      "-d, --data       HTTP POST data\n"
+      "-H, --header     Add header to request\n"
+      "-u, --url        Specify url (optional)\n"
+      "-x, --post       Specify request as a POST message(optional)\n"
+      "-v, --verbose    Show more information on request\n"
+  );
+}
+
 int main(int argc, char **argv) {
   int ret, opt;
-  char *message, *data;
+  char *message = NULL;
+  char *data = NULL;
   char *headers = malloc(512);
   struct URL_Components* components;
 
@@ -20,11 +32,17 @@ int main(int argc, char **argv) {
       {"verbose", no_argument, &verbose_flag, 0},
       {"post", no_argument, &post_flag, 1},
       {"post", no_argument, &post_flag, 0},
+      {"help", no_argument, NULL, 'h'},
       {"header", required_argument, NULL, 'H'},
       {"data", required_argument, NULL, 'd'},
       {"url", required_argument, NULL, 'u'},
       {0, 0, 0, 0}
     };
+
+  if (argc < 2) {
+    usage();
+    return 0;
+  }
 
   while (1) {
     int opt_index = 0;
@@ -42,6 +60,10 @@ int main(int argc, char **argv) {
           printf (" with arg %s", optarg);
         printf ("\n");
         break;
+
+      case 'h':
+        usage();
+        return 0;        
 
       case 'H':
         strcat(headers, optarg);
@@ -84,8 +106,6 @@ int main(int argc, char **argv) {
   } else {
     message = generate_get_request_message(components);
   }
-
-  free(data);
 
   if (strcmp(components->scheme, "https") == 0) {
     ret = make_https_request(components, message);
